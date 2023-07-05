35ae4202054b7dc97ae0923a06d7a9dca0f903e6
jujutrald <129910359+jujutrald@users.noreply.github.com>
2023-07-04 21:51:41 -0300

    we never go out of style
    é sobre o harry =)))))))))))))))
    the office é mega bom.joia

---
+++ get element by name/querySelectorAll.js
@@ -0,0 +1,3 @@
+let texo = document.querySelector('.texto');
+
+console.log(texto);
---
+++ styles/styles.js
@@ -0,0 +1,4 @@
+let texto = document.getElementById("texto");
+
+texto.style.color = "#8B008B";
+texto.style.fontSize = "50px";

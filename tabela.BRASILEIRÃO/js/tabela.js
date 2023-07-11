be10666459ca437f2d56e32f972d6d99a375c1d8
jujutrald <129910359+jujutrald@users.noreply.github.com>
2023-07-10 21:55:07 -0300

    eh bom ter musicas na cabeça
    por exemplo, flashing lights e company, muitos.joias=)

--- tabela.BRASILEIRÃO/js/tabela.js
+++ tabela.BRASILEIRÃO/js/tabela.js
@@ -1,4 +1,4 @@
-function createGame(player1, hour, player2){
+function createGame(player1, hour, player2) {
     return `
     <li>
     <img src="./times/icon-${player1}.png" alt="Bandeira do ${player1}"/>
@@ -7,8 +7,8 @@ function createGame(player1, hour, player2){
     `
 }
 let delay = -0.4;
-function createCard(date, day, games){
-    delay = delay +0.4;
+function createCard(date, day, games) {
+    delay = delay + 0.4;
     return `
     <div class="card" style="animation-delay:${delay}s">
     <h2>${date} <span>${day}</span></h2>
@@ -18,5 +18,7 @@ function createCard(date, day, games){
     </div>
     `
 }
-document.querySelector("#cards").innerHTML = 
-createCard("05/07", "Quarta-Feira", createGame("corinthians", "21:00", "palmeiras"))
+document.querySelector("#cards").innerHTML =
+    createCard("05/07", "Quarta-Feira", createGame("corinthians", "21:00", "palmeiras")) + createCard("10/07", "Segunda-Feira", createGame("atletico", "20:00", "bahia")) + 
+createCard("12/07", "Quarta-Feira", createGame("sp", "19:00", "vasco")) +createCard("14/07", "Sexta-Feira", createGame("inter", "18:00", "santos")) + createCard("16/07", "Domingo", createGame("bota", "16:00", "flamengo")) + 
+createCard("20/07", "Quinta-Feira", createGame("cruzeiro", "21:00", "gremio"))
